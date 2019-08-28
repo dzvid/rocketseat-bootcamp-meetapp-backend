1a0b68f9df62fcf703615f1b150f6521aa76d1d5
tukno <oliveiradavid007@gmail.com>
2019-08-27 21:57:06 -0400

    chore: Encrypt user password - Using library bcryptjs with a salt = 10;

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,21 +5,22 @@ class UserController {
   // Cadastro de usuário (POST /users)
   async store(req, res) {
     // TODO - Validate input
+
+    // SAVE USER TO DATABASE
     // Get user data from request body
     const { name, email, password } = req.body;
 
-    // SAVE USER TO DATABASE
     // Checks if user already exists (verify if user email is in the database)
     const userExists = await User.findOne({ where: { email } });
 
-    // If user exists, returns error message
+    // If user exists, returns error message to client
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
     // If user does not exist, register user in the application
-    // and returns some user info to frontend (at the moment to generate session token)
+    // and returns some user info to client (at the moment to generate session token)
     // { name, email, password }) -> req.body
-    const user = await User.create({ name, email, password_hash: password });
+    const user = await User.create({ name, email, password });
 
     return res.json({ id: user.id, name, email });
   }
