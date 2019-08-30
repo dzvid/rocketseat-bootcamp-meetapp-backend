61053db643517b32777720a648e31c4f0eb7c196
tukno <oliveiradavid007@gmail.com>
2019-08-30 18:46:25 -0400

    chore: Validate user input information in update method - Using Yup library, there are still cases where the validation fails, need to revise the use cases.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,3 +1,5 @@
+import * as Yup from 'yup';
+
 // Import User model
 import User from '../models/User';
 
@@ -28,8 +30,33 @@ class UserController {
   // Update user information (PUT /users)
   async update(req, res) {
     // TODO - Verify if inputs are really valid
+    // Example: if oldPassword is not presented then password.when() should disable the field or something
+
+    // Verify if inputs are 'ok'
+    // Create a object schema
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string()
+        .min(6)
+        .when('password', (password, field) =>
+          password ? field.required().oneOf([Yup.ref('password')]) : field
+        ),
+    });
+
+    // Validates input using Yup
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     // Get user information from request
-    const { email, oldPassword } = req.body;
+    const { email, oldPassword, password, confirmPassword } = req.body;
 
     // Retrieve current user from database
     // Check if user still exists in database
@@ -44,8 +71,13 @@ class UserController {
       }
     }
 
-    // Verify if user password matchs current password
+    // Verify if user password is falsy (empty, undefined (was not passed on request)
+    // this should be have done by Yup)
     // TODO - Verify if user wants to change password
+    if (!oldPassword && (password || confirmPassword)) {
+      return res.status(401).json({ error: 'Password is required.' });
+    }
+    // Verify if user password matchs current password
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
